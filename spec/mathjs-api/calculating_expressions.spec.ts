74877c9f67e5ae4c0df4005bfdcbdb93b9db64f7
viper3400 <viper_3400@gmx.de>
2022-06-07 16:24:19 +0200

    initalized actors by names in before

--- spec/mathjs-api/calculating_expressions.spec.ts
+++ spec/mathjs-api/calculating_expressions.spec.ts
@@ -2,7 +2,7 @@
 import 'mocha';
 
 import { Ensure, equals } from '@serenity-js/assertions';
-import { actorCalled, engage, notes } from '@serenity-js/core';
+import { actorCalled, engage, Log, notes } from '@serenity-js/core';
 import { GetRequest, LastResponse, Send } from '@serenity-js/rest';
 import { escape } from 'querystring';
 import { Actors, CalculationResult } from '../../src/index';
@@ -10,8 +10,9 @@ import { Actors, CalculationResult } from '../../src/index';
 describe('Math.js API', () => {
 
     before( () => {
-        const actors = new Actors(process.env.BASE_API_URL || 'http://api.mathjs.org/v4/')
-        engage(actors);
+        engage(new Actors(process.env.BASE_API_URL || 'http://api.mathjs.org/v4/')),
+        actorCalled('Apisitt').attemptsTo(Log.the(notes().toJSON())),
+        actorCalled('Jan').attemptsTo(Log.the(notes().toJSON()))
     })
 
     describe('GET /v4/?expr', () => {
@@ -21,7 +22,7 @@ describe('Math.js API', () => {
             GetRequest.to(`/v4?expr=${ escape(expression) }`)
                 .describedAs(`a request to calculate ${ expression }`)
 
-        it('supports calculating a single expression', () =>
+        it('supports Apisitt with calculating a single expression', () =>
             actorCalled('Apisitt').attemptsTo(
                 Send.a(RequestToCalculateExpression('2 + 2')),
                 Ensure.that(notes().get('additionResult'), equals(0)),
@@ -30,6 +31,17 @@ describe('Math.js API', () => {
                 Ensure.that(LastResponse.body<number>(), equals(4)),
                 Ensure.that(notes().get('addition result'), equals(4)),
                 Ensure.that(notes<CalculationResult>().get('additionResult'), equals(4))
+            ));
+
+         it('supports Apisitt with calculating a single expression', () =>
+            actorCalled('Jan').attemptsTo(
+                Send.a(RequestToCalculateExpression('2 + 3')),
+                Ensure.that(notes().get('additionResult'), equals(0)),
+                notes<CalculationResult>().set('additionResult', LastResponse.body<number>()),
+                notes().set('addition result', LastResponse.body<number>()),
+                Ensure.that(LastResponse.body<number>(), equals(5)),
+                Ensure.that(notes().get('addition result'), equals(5)),
+                Ensure.that(notes<CalculationResult>().get('additionResult'), equals(5))
             ));
 
         it('remembers Apisitt`s untyped notes', () =>
@@ -41,6 +53,21 @@ describe('Math.js API', () => {
             actorCalled('Apisitt').attemptsTo(
                 Ensure.that(notes<CalculationResult>().get('additionResult'), equals(4))
           ));
+
+        it('remembers Jan`s untyped notes', () =>
+          actorCalled('Jan').attemptsTo(
+          Ensure.that(notes().get('addition result'), equals(5))
+          ));
+
+        it('remembers Jans`s typed notes', () =>
+          actorCalled('Jan').attemptsTo(
+              Ensure.that(notes<CalculationResult>().get('additionResult'), equals(5))
+        ));
     });
+
+    after( () => {
+        actorCalled('Apisitt').attemptsTo(Log.the(notes().toJSON())),
+        actorCalled('Jan').attemptsTo(Log.the(notes().toJSON()))
+    })
 
 });
